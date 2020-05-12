6994ddde301b88c9cfe20a6a448cec7cc43f4941
arikankainen <60932647+arikankainen@users.noreply.github.com>
2020-05-12 16:35:44 +0300

    8.8: Kirjailijoiden näkymä

---
+++ library-frontend/src/components/Authors.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+import { ALL_AUTHORS } from '../queries'
+import { useQuery } from '@apollo/client'
+
+const Authors = (props) => {
+  const [authors, setAuthors] = useState([])
+  const result = useQuery(ALL_AUTHORS)
+  
+  useEffect(() => {
+    if (result.data) {
+      setAuthors(result.data.allAuthors)
+    }
+  }, [result.data])
+  
+  if (!props.show) {
+    return null
+  }
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
+
+export default Authors
